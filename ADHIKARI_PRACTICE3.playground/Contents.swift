8c13814b1f9a0928cb61087ae9e4e3bfdf9fd331
Bishal Adhikari <99350850+dearbishal@users.noreply.github.com>
2023-08-24 13:28:31 -0500

    done

---
+++ ADHIKARI_PRACTICE3.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var mobileBrand = "Apple"
+mobileBrand = "Samsung"
+print (mobileBrand)
+
+let pi = 3.14
+print (pi)
+
+var age : Int = 23
+age = age * 2
+print(age)
+
+var aweMessage = "This is Superb!"
+print(aweMessage)
+print("aweMessage")
+
+var course1 = "iOS"
+var course2 = "Java"
+print(course1,course2)
+print(course1, "-", course2)
+
+print(10,20,30)
+print(12.5,15.5)
---
+++ AdhikariPractice-Playgraound.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print ("Hello, Moon!")
+print("Hii",10,12.25)
+
+var programmingLanguage = "Swift"
+print("My favourite programming language is \(programmingLanguage)")
+
+var age = 23
+print ("You are \(age) years old and in another \(age) years, you will be \(age * 2)")
+
+//print(""" Hello World! "")
+print("""
+      Hello World!
+""")
+
+print ("Hello ALL, \rWelcome to Swift programming")
+
+let welcomeMessage : String = "Hello!"
+print(welcomeMessage , "ALL")
+
+print("Welcome to Swift Programming")
+print("Fall 2023")
+print("************")
+print("Welcome to Swift Programming" , terminator : "-")
+print("Fall 2023")
+
+print("The list of numbers are ")
+print(1,2,3,4,5,6)
+print("The new pattern is")
+print(1,2,3,4,5,6, separator: "-")
+
+
